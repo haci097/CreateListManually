1216dc6ebb2d22b6922b9797cb2c345eb6fc6eaf
Haci <Haci@DESKTOP-TM0EIQE>
2022-10-08 01:05:34 +0400

    Add project files.

---
+++ CreateListManually/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CreateListManually;
+
+MyList<string> values = new MyList<string>();
+values.Add("Phyton");
+values.Add("C#");
+values.Add("Java");
+
+foreach (var item in values.Items)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine();
+
+Console.WriteLine("Count: " + values.Count);
